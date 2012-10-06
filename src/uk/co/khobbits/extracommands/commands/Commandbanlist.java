eebd1223412630c06209b97767467ec8dd47d7bc
KHobbits <rob@khobbits.co.uk>
2012-10-06 22:13:39 +0100

    add banlist command

---
+++ src/uk/co/khobbits/extracommands/commands/Commandbanlist.java
@@ -0,0 +1,37 @@
+package uk.co.khobbits.extracommands.commands;
+
+import com.earth2me.essentials.User;
+import com.earth2me.essentials.Util;
+import java.util.Set;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import uk.co.khobbits.extracommands.CommandException;
+import uk.co.khobbits.extracommands.ExtraCommand;
+
+public class Commandbanlist extends ExtraCommand {
+
+    public Commandbanlist() {
+        super();
+    }
+
+    @Override
+    public void run(Server server, CommandSender sender, Command cmd, String label, String[] args) throws CommandException {
+        Set<OfflinePlayer> banlist = server.getBannedPlayers();
+
+        sender.sendMessage("Banned player list:");
+        for (OfflinePlayer bannedplayer : banlist) {
+            User banneduser = getEss().getUser(bannedplayer.getName());
+            if (banneduser == null) {
+                sender.sendMessage("Player: " + bannedplayer.getName() + " Reason: unknown");
+                continue;
+            }
+            if (banneduser.getBanTimeout() != 0) {
+                sender.sendMessage("Player: " + bannedplayer.getName() + " Expires: " + Util.formatDateDiff(banneduser.getBanTimeout()) + " Reason: " + banneduser.getBanReason());
+            } else {
+                sender.sendMessage("Player: " + bannedplayer.getName() + " Reason: " + banneduser.getBanReason());
+            }
+        }
+    }
+}
